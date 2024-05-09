688277e17f3f8224b79acceeb643760c546e1b23
mohanned-sameh <mohanned2678@gmail.com>
2024-05-10 01:46:24 +0300

    checking input for size of board

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
 import random
 
-N = 9
-
 
 def printSolution(board):
     """A utility function to print the solution"""
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=" ")
-        print()
+    for row in board:
+        print(" ".join(map(str, row)))
 
 
 def isSafe(board, row, col, num):
     """Check whether it will be legal to assign num to the given row, col"""
-    for x in range(N):
-        if board[row][x] == num:
+    n = len(board)
+    sqrt_n = int(n**0.5)
+
+    # Check row and column for duplicates
+    for x in range(n):
+        if board[row][x] == num or board[x][col] == num:
             return False
 
-    for x in range(N):
-        if board[x][col] == num:
-            return False
-
-    startRow = row - row % 3
-    startCol = col - col % 3
-    for i in range(3):
-        for j in range(3):
+    # Check subgrid for duplicates
+    startRow = row - row % sqrt_n
+    startCol = col - col % sqrt_n
+    for i in range(sqrt_n):
+        for j in range(sqrt_n):
             if board[i + startRow][j + startCol] == num:
                 return False
     return True
@@ -35,17 +32,17 @@ def solveSudoku(board, row, col):
     """Takes a partially filled-in grid and attempts to assign values to all
     unassigned locations in such a way to meet the requirements for
     Sudoku solution (non-duplication across rows, columns, and boxes)"""
-    if row == N - 1 and col == N:
+    n = len(board)
+    if row == n:
         return True
 
-    if col == N:
-        row += 1
-        col = 0
+    if col == n:
+        return solveSudoku(board, row + 1, 0)
 
     if board[row][col] > 0:
         return solveSudoku(board, row, col + 1)
 
-    nums = random.sample(range(1, N + 1), N)
+    nums = random.sample(range(1, n + 1), n)
     for num in nums:
         if isSafe(board, row, col, num):
             board[row][col] = num
@@ -55,7 +52,7 @@ def solveSudoku(board, row, col):
     return False
 
 
-def generateRandomBoard():
+def generateRandomBoard(N):
     """Generate a random valid Sudoku board"""
     board = [[0 for _ in range(N)] for _ in range(N)]
     solveSudoku(board, 0, 0)
@@ -64,6 +61,12 @@ def generateRandomBoard():
 
 if __name__ == "__main__":
     """Main function to test above functions"""
-    board = generateRandomBoard()
-    print("Randomly generated Sudoku board:")
+    N = int(input("Enter the size of the Sudoku board (N x N): "))
+    if N <= 0:
+        print("N should be greater than 0")
+        exit(1)
+    elif N % int(N**0.5) != 0:
+        print("N should be a perfect square")
+        exit(1)
+    board = generateRandomBoard(N)
     printSolution(board)
